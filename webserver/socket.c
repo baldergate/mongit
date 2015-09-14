4a85a1c97698fd03e53ed402b64fef7cd0e32c0d
Maximilien BONNARD <maximilien.bonnard@etud.univ-lille1.fr>
2015-09-14 17:34:37 +0200

    transfo main.c rm main.c/.o ajout socket

---
+++ webserver/socket.c
@@ -0,0 +1,48 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include "socket.h"
+
+int creer_serveur(int port){
+	int socket_serveur;
+
+	socket_serveur = socket(AF_INET,SOCK_STREAM,0);
+	if (socket_serveur ==-1){
+
+		perror("socket_serveur");		/*traitement de l'erreur*/
+
+	}/* Utilisation de la socket*/
+
+	struct sockaddr_in saddr;
+	saddr.sin_family = AF_INET;		/* Socket ipv4 */
+	saddr.sin_port = htons(port);	/* Port d’écoute */
+	saddr.sin_addr.s_addr = INADDR_ANY;	/* écoute sur toutes les interfaces */
+
+	if (bind(socket_serveur , (struct sockaddr *)&saddr , sizeof(saddr)) == -1)
+	{
+		perror("bind socker_serveur");	/* traitement de l’erreur */
+	}
+
+	if (listen(socket_serveur , 10) == -1)
+	{
+		perror("listen socket_serveur");	/* traitement d’erreur */
+	}
+
+	int socket_client;
+	socket_client = accept(socket_serveur , NULL , NULL);
+	if (socket_client == -1)
+	{
+		perror("accept");	/* traitement d’erreur */
+	}
+	
+	/* On peut maintenant dialoguer avec le client */
+	const char *message_bienvenue = "Bonjour , bienvenue sur notre  serveur batard\n";
+	return write(socket_client , message_bienvenue , strlen(message_bienvenue));
+
+
+}
